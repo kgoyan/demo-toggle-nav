932d9ab64bd0f2707749d778692d40cc2f69e0bf
kgoyan <kaingoyan@gmail.com>
2021-03-01 18:43:03 -0700

    Day one.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-// js code to trigger the button goes here
+// Step 1: query the document to find the element we will be clicking on 
+const button = document.querySelector('.hamburger-btn');
+
+// Step 2: add a click event listener on the button 
+button.addEventListener('click', () => {
+    // when the button is clicked, grab the nav element and add the special class 
+    document.querySelector('nav').classList.toggle('show-nav');
+});
